ab1e41af7184072dd4d11bec380317757b979bc6
Somit17 <somitmeshram3256@gmail.com>
2025-12-17 22:37:54 +0530

    Prime-React Setup and SQL-Installation in Docker

--- frontend/todo-ui/src/App.tsx
+++ frontend/todo-ui/src/App.tsx
@@ -1,13 +1,21 @@
-import './App.css'
+import { Button } from 'primereact/button';
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      Basic Set-up
-        
-    </>
-  )
+    <div className="App">
+      <div className="card">
+        <Button
+          icon="pi pi-plus"
+          className="mr-2"
+          label="Increment"
+        ></Button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/todo-ui/src/main.tsx
+++ frontend/todo-ui/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { PrimeReactProvider } from "primereact/api";
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <PrimeReactProvider>
+       <App />
+    </PrimeReactProvider>
+  </React.StrictMode>
 )
